519d122ca8a0cb5da6f9abbac7fda309cfd21ffb
Tateishi Tadatoshi <tateishi@tw.tokai-tv.co.jp>
2021-03-22 15:44:08 +0900

    package path を変更

--- adder/__init__.py
+++ adder/__init__.py
@@ -1,6 +1,8 @@
 # -*- mode: python; coding: utf-8 -*-
 
 
-from .core import *
-
 __version__ = '0.0.1'
+
+
+from . import core
+add = core.add
--- adder/__main__.py
+++ adder/__main__.py
@@ -1,7 +1,7 @@
 # -*- mode: python; coding: utf-8 -*-
 
 
-from adder import add
+from . import add
 
 
 def main():
